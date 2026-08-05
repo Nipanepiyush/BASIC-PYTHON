73f4068ec160a412c41ae47098ce026cfc75903d
Nipanepiyush <piyushnipane74@gmail.com>
2026-08-06 00:46:30 +0530

    Currency Converter Code

---
+++ Basic/Currency-Converter.py
@@ -0,0 +1,8 @@
+print("Curreny Conevrter")
+Amount = float(input("Enter your Amount:"))
+
+Doller = Amount * 0.011
+Eurose = Amount * 0.0091
+
+print("Your amount in Doller:", (Doller),"$")
+print("Your amount in EUrose:", (Eurose),"€")
